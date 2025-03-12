3acdf7af5d67e4dd2313cee7bb3abefa0dac5daf
Haikal Fikri <haikaljr1245@gmail.com>
2025-03-12 15:01:20 +0700

    connected login and register with backend api, setup router to check role and token

--- frontend/src/views/auth/LoginPage.vue
+++ frontend/src/views/auth/LoginPage.vue
@@ -8,17 +8,17 @@
                         class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                         Sign In
                     </h1>
-                    <form class="space-y-4 md:space-y-6" action="#">
+                    <form @submit.prevent="Login" class="space-y-4 md:space-y-6" action="#">
                         <div>
                             <label for="email" class="block mb-2 text-sm text-left font-medium">Email address</label>
-                            <input type="email" name="email" id="email"
+                            <input v-model="email" type="email" name="email" id="email"
                                 class="border border-gray-[CFD5E3] text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="name@example.com" required>
                         </div>
                         <div>
                             <label for="password"
                                 class="block mb-2 text-sm text-left font-medium">Password</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••"
+                            <input v-model="password" type="password" name="password" id="password" placeholder="••••••••"
                                 class="border border-gray-[CFD5E3] text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 required>
                         </div>
@@ -36,7 +36,41 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: "LoginPage",
+    data() {
+        return {
+            email: "",
+            password: "",
+        }
+    },
+    methods: {
+        async Login() {
+            try {
+                const response = await axios.post("http://localhost:8000/api/v1/login", {
+                    email: this.email,
+                    password: this.password,
+                });
+
+                const token = response.data.access;
+                const role = response.data.role;
+
+                // store token and role to localStorage
+                localStorage.setItem('accessToken', token);
+                localStorage.setItem('role', role);
+
+                // redirect based on role
+                if (role == 'user') {
+                    this.$router.push("/user/cryptoPage");
+                } else {
+                    this.$router.push("/admin/cryptoPage");
+                }
+            } catch (error) {
+                console.log("Login failed. Check your credentials");
+            }
+        }
+    }
 }
 </script>
