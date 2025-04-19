1bf5b90feddade6188ca60202c34bfa0a5d0c2f8
Haikal Fikri <haikaljr1245@gmail.com>
2025-04-19 09:40:42 +0700

    membuat loading button pada saat pemrosesan data

--- frontend/src/components/PredictButton.vue
+++ frontend/src/components/PredictButton.vue
@@ -18,15 +18,24 @@
         </select>
 
         <!-- Search Button -->
-        <button type="submit"
+        <button type="submit" :disabled="loading"
             class="inline-flex items-center py-2.5 px-3 text-sm font-medium text-white bg-blue-700 rounded-2xl border border-blue-700
-        hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                viewBox="0 0 20 20">
+    hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+
+            <!-- Loading Spinner -->
+            <svg v-if="loading" class="animate-spin h-4 w-4 text-white mr-2" xmlns="http://www.w3.org/2000/svg"
+                fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
+            </svg>
+
+            <!-- Search Icon (Hide when loading) -->
+            <svg v-else class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
             </svg>
-            Search
+
+            <span>{{ loading ? 'Loading...' : 'Search' }}</span>
         </button>
     </form>
 
