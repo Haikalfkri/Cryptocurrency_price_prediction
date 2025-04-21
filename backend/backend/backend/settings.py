042f0c8b9cb7f4ba66112f13e71c846674e409d3
Haikal Fikri <haikaljr1245@gmail.com>
2025-04-21 13:26:03 +0700

    add gunicorn to requirements.txt

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
+import dj_database_url
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -94,6 +96,7 @@ DATABASES = {
     }
 }
 
+DATABASES["default"] = dj_database_url.parse(os.getenv("DJANGO_DB_URL"))
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
